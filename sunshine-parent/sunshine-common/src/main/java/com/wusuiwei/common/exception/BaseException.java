504d98306ffc0785bee06f210c7c971a8bd63f8e
wusuiwei <1732405580@qq.com>
2021-08-02 19:15:36 +0800

    基础异常接口

---
+++ sunshine-parent/sunshine-common/src/main/java/com/wusuiwei/common/exception/BaseException.java
@@ -0,0 +1,26 @@
+package com.wusuiwei.common.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 基础异常接口
+ *
+ * @author SUIWEI WU
+ * @date 2021/8/2 19:12
+ */
+public interface BaseException {
+    long getErrorCode();
+
+    String getMessage();
+
+    String getException();
+
+    default Map<String, Object> getErrorMap() {
+        Map<String, Object> map = new HashMap<>(3);
+        map.put("code", this.getErrorCode());
+        map.put("msg", this.getMessage());
+        map.put("exception", this.getException());
+        return map;
+    }
+}
--- sunshine-parent/sunshine-common/src/main/java/com/wusuiwei/common/utils/Results.java
+++
@@ -1,8 +0,0 @@
-package com.wusuiwei.common.utils;
-
-/**
- * @author SUIWEI WU
- * @date 2021/8/2 16:21
- */
-public class Results {
-}
